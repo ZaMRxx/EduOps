9c9aa529c5e6e72a5b6fe529f2530ff866d08e3b
Rafli Zamzami <raflizamzami001@gmail.com>
2026-06-14 16:59:52 +0700

    Serve uploaded media on Railway demo

--- eoas/urls.py
+++ eoas/urls.py
@@ -1,12 +1,9 @@
-"""
-URL configuration for eoas project.
-Sistem Manajemen Operasional & Absensi Educourse (EOAS).
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
@@ -17,4 +14,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
